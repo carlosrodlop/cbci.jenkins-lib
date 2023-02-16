7bc22721a38886086c87a4899bca55cf2d568245
Carlos Rodriguez Lopez <it.carlosrodlop@gmail.com>
2023-02-16 16:21:02 +0100

    cleaning sensitive info

--- vars/thirdPartyLib.groovy
+++ vars/thirdPartyLib.groovy
@@ -1,5 +1,4 @@
 // it needs to be maven type
-// @GrabResolver(name='artifactory', root='http://artifactory-oss.dse-beescloud.com:80/artifactory/my-repo-libs-release-local/', m2Compatible=true)
 @Grab('org.apache.commons:commons-math3:3.4.1')
 import org.apache.commons.math3.primes.Primes
 
