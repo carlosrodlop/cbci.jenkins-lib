305a2b7503a36e7e3b116832bfeb29cc0e032fa9
Carlos Rodriguez Lopez <it.carlosrodlop@gmail.com>
2023-02-28 14:50:02 +0100

    cleaning main from pipelines items

--- vars/strNoSpacesValidator.groovy
+++
@@ -1,6 +0,0 @@
-// vars/gitUrlValidator.groovy
-def call(paramName, paramValue) {
-  if (paramValue.contains(" ")){
-      error("${paramName} is not expecting any spaces within the string")
-  }
-}
