f07f3f7dfd4affd963f6a27f6a946682242bf964
Carlos Rodriguez Lopez <it.carlosrodlop@gmail.com>
2023-02-28 11:26:04 +0100

    Update enableJobs.groovy

--- src/script/jenkins/jobs/enableJobs.groovy
+++ src/script/jenkins/jobs/enableJobs.groovy
@@ -7,12 +7,15 @@ package script.jenkins.jobs
 import jenkins.model.*
 import com.cloudbees.hudson.plugins.folder.AbstractFolder
 
-ArrayList folders = ["folder-a","folder-b"]
+ArrayList folders = ["folderA", "folderB"]
 dryRun=false //true (mock) or false (real)
 disable=false //true (disable) or false (enable)
+
 jenkins=Jenkins.instanceOrNull
+validPathsHelp=false
 
 for (String folder : folders) {
+  if (jenkins.getItemByFullName(folder, AbstractFolder)) {
     jenkins.getItemByFullName(folder, AbstractFolder).getAllItems()
             .findAll { it instanceof ParameterizedJobMixIn.ParameterizedJob || it instanceof AbstractFolder }
             .each {
@@ -21,5 +24,15 @@ for (String folder : folders) {
                 }
                 println("Job [$it.fullName] set Disable to $disable")
             }
-    null
+  } else {
+    println "Folder $folder is not a valid path."
+    validPathsHelp=true
+  }
 }
+if (validPathsHelp){
+    println "Valid folders paths are:"
+    jenkins.getAllItems(AbstractFolder.class).each {it ->
+    	println " - $it.fullName";
+	}
+ }
+null
