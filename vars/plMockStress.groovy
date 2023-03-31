cbfb75b9e6fb1c8ee063ab9c125833fc4f404d73
Carlos Rodriguez Lopez <it.carlosrodlop@gmail.com>
2023-03-31 18:55:22 +0200

    test with stress command tool

---
+++ vars/plMockStress.groovy
@@ -0,0 +1,42 @@
+def call(configYaml) {
+
+    Map config = readYaml text: "${configYaml}"
+
+   pipeline {
+        environment {
+            SLEEP_SEC = "${config.sleepTime}"
+        }
+        agent {
+            kubernetes {
+                yaml '''
+                    apiVersion: v1
+                    kind: Pod
+                    spec:
+                        containers:
+                        - name: stress
+                          image: carlosrodlop/stress.ubuntu.ub:main
+                          command:
+                          - cat
+                          tty: true
+                        '''
+                    retries 2
+                    defaultcontainer 'stress'
+                    }
+                }
+        triggers {
+            eventTrigger jmespathQuery("eventName=='${config.eventName}'")
+        }
+        stages {
+            stage('Stress CPU') {
+                steps {
+                    sh """
+                        uptime
+                        sudo stress --cpu  8 --timeout 20
+                        uptime
+                    """
+                    }
+                }
+            }
+        }
+    }
+}
